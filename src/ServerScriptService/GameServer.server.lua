80aee7ad45f18ab98d93bc580673d43c38928048
CoolShank8 <Shankbamo8@gmail.com>
2025-10-11 02:57:22 -0700

    added round system

--- src/ServerScriptService/GameServer.server.lua
+++ src/ServerScriptService/GameServer.server.lua
@@ -1,8 +1,14 @@
+local MinimumAmountOfPlayersNeeded = 1
+
 while true do
     local CurrentPlayers = game:GetService('Players'):GetPlayers()
+	local GamemodeChoosen = require(game.ServerScriptService.GameModes:GetChildren())
+	
+    print('Teleporting players soon ')
+    task.wait(4)
 
-    if (#CurrentPlayers >= 2) then
-      
+    if (#CurrentPlayers >= MinimumAmountOfPlayersNeeded) then
+  		   GamemodeChoosen:Start()
     else
         print('Waiting for more players to join...')
     end
