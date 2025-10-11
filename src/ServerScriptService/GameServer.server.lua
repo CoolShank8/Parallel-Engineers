c3546043441375b250a9d30f3afefc363d895cf2
CoolShank8 <Shankbamo8@gmail.com>
2025-10-11 03:36:02 -0700

    updated plot system and zombie system

--- src/ServerScriptService/GameServer.server.lua
+++ src/ServerScriptService/GameServer.server.lua
@@ -2,13 +2,14 @@ local MinimumAmountOfPlayersNeeded = 1
 
 while true do
     local CurrentPlayers = game:GetService('Players'):GetPlayers()
-	local GamemodeChoosen = require(game.ServerScriptService.GameModes:GetChildren())
+	local AllGameModes = game.ServerScriptService.GameModes:GetChildren()
+	local GameModeChoosen = require(AllGameModes[math.random(1, #AllGameModes)])
 	
     print('Teleporting players soon ')
     task.wait(4)
 
     if (#CurrentPlayers >= MinimumAmountOfPlayersNeeded) then
-  		   GamemodeChoosen:Start()
+  		GameModeChoosen:Start()
     else
         print('Waiting for more players to join...')
     end
