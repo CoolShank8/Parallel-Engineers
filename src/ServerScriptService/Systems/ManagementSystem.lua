c6018d45adb0a9360c3d319b240b0cda9a8c04f9
CoolShank8 <Shankbamo8@gmail.com>
2025-10-10 22:33:47 -0700

    started coding managemt system

---
+++ src/ServerScriptService/Systems/ManagementSystem.lua
@@ -0,0 +1,13 @@
+print 'adding maanagement system'
+
+type ManagementSystem = {
+    Name: string,
+    Version: number,
+}
+
+local ManagementSystem: ManagementSystem = {
+    Name = "ManagementSystem",
+    Version = 1.0,
+}
+
+ManagementSystem.Name = 'something else'
