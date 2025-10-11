2ea88245e0ade23950cb755f046abf4a8b66ea90
CoolShank8 <Shankbamo8@gmail.com>
2025-10-10 22:36:26 -0700

    Update ManagementSystem.lua

--- src/ServerScriptService/Systems/ManagementSystem.lua
+++ src/ServerScriptService/Systems/ManagementSystem.lua
@@ -10,4 +10,9 @@ local ManagementSystem: ManagementSystem = {
     Version = 1.0,
 }
 
+for i,v in game:GetService('Players'):GetPlayers()
+do
+    print('hello ' .. v.UserId)
+end
+
 ManagementSystem.Name = 'something else'
